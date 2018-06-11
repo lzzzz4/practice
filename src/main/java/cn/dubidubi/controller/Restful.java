abb1be1cd417cdced0775a5c030e78d27c1a7ad5
lzj0726 <1622472966@qq.com>
2018-06-11 16:28:42 +0800

    1、新增restful api

---
+++ src/main/java/cn/dubidubi/controller/Restful.java
@@ -0,0 +1,27 @@
+package cn.dubidubi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lzj
+ * @Description:
+ * @date 18-6-11下午3:28
+ */
+@Controller
+@RequestMapping("friend")
+public class Restful {
+
+    @ModelAttribute("prepare")
+    public String prepare() {
+        return "hello";
+    }
+
+    @RequestMapping("/v1/{sb}")
+    @ResponseBody
+    public String show(@PathVariable String sb, @ModelAttribute("prepare") String s2) {
+        System.out.println("sb" + sb);
+        System.out.println("prepare" + s2);
+        return "hello";
+    }
+}
---
+++ src/main/java/cn/dubidubi/controller/Restful2.java
@@ -0,0 +1,18 @@
+package cn.dubidubi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author lzj
+ * @Description:
+ * @date 18-6-11下午3:48
+ */
+@Controller
+@RequestMapping("/no/v1")
+public class Restful2 {
+    @RequestMapping("/show")
+    public void show() {
+        System.out.println("show");
+    }
+}
