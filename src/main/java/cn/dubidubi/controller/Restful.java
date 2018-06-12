833b2dad38ea244c87ea695d69dd2ef2941f10ad
lzj0726 <1622472966@qq.com>
2018-06-12 10:27:59 +0800

    1、测试乱码

--- src/main/java/cn/dubidubi/controller/Restful.java
+++ src/main/java/cn/dubidubi/controller/Restful.java
@@ -1,27 +1,1 @@
-package cn.dubidubi.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-/**
- * @author lzj
- * @Description:
- * @date 18-6-11下午3:28
- */
-@Controller
-@RequestMapping("friend")
-public class Restful {
-
-    @ModelAttribute("prepare")
-    public String prepare() {
-        return "hello";
-    }
-
-    @RequestMapping("/v1/{sb}")
-    @ResponseBody
-    public String show(@PathVariable String sb, @ModelAttribute("prepare") String s2) {
-        System.out.println("sb" + sb);
-        System.out.println("prepare2" + s2);
-        return "hello";
-    }
-}
+package cn.dubidubi.controller;import cn.dubidubi.model.User;import org.apache.commons.lang3.CharSet;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.*;import cn.dubidubi.dao.chanMapper;import java.io.UnsupportedEncodingException;import java.nio.charset.Charset;/** * @author lzj * @Description: * @date 18-6-11下午3:28 */@Controller@RequestMapping("friend")public class Restful {    @Autowired    chanMapper chanMapper;    @ModelAttribute("prepare")    public String prepare() {        return "你好";    }    @RequestMapping("/v1/{sb}")    @ResponseBody    public User show(@PathVariable String sb, String demo, @ModelAttribute("prepare") String s2) throws UnsupportedEncodingException {        System.out.println("sb" + sb);        System.out.println("prepare2" + s2);        String str = new String("胡浩".getBytes("gbk"), "utf-8");        User User = new User();        System.out.println(chanMapper.getTitle(2));        User.setUsername(str);        System.out.println("demo" + demo);        User.setDemo(demo);        return User;    }    public static void main(String[] args) {        System.out.println(Charset.defaultCharset());    }}
